14f352ac928f78c071fd32652012f6e0b49745b4
Basem Rizk <basem.rizk@outlook.com>
2018-11-07 22:54:45 +0100

    Project cleaned and submitted as it appears, including Sound effects files

--- MadCar_DMET/Helpers.h
+++
@@ -1,6 +0,0 @@
-/* This file contain all creation and generation functions and structs of Obstacles and Powerups*/
-#ifndef HELPERS_H
-#define HELPERS_H
-
-// This is the end of the header guard
-#endif
